3a32a6c6ca836d32c10b272e96132ce7b2d8dc68
Huỳnh Trọng Viên <huynhtrongvien2005@gmail.com>
2024-05-15 00:02:44 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,11 @@
 #include"Functions.h"
 
-int main()
-{
-	cout << "HELLO WORLD!" << endl;
-	return 0;
+int main() {
+	string file_name[] = { "23CTT1.txt", "23CTT2.txt", "23CTT3.txt", "23CTT4.txt", "23CTT5.txt", "23CLC01.txt", "23CLC02.txt", "23APCS1.txt", "23APCS2.txt" };
+	int n1 = chooseClass();
+	AcaYear aca = createAcademicYear(2023);
+	int sl;
+	Clas* a= createClass(file_name, aca, sl, n1);
+	printAcaYear(aca);
+		return 0;
 }
