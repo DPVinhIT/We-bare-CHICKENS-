e3b0d16aabfb509fdc2e6e5dc9e3efd8d659335d
Huỳnh Trọng Viên <huynhtrongvien2005@gmail.com>
2024-05-14 23:55:56 +0700

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -1,1 +1,17 @@
 #include"Functions.h"
+//in ra màn hình học sinh
+void printStudent(Student* st, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << st[i].studentID << '-'<< st[i].fullName << '-' << st[i].Gender << '-' 
+			<< st[i].DoB.day << '/' << st[i].DoB.month << '/' << st[i].DoB.year << '-' << st[i].socialID << endl;
+	}
+}
+//in ra màn hình thông tin năm học
+void printAcaYear(AcaYear aca) {
+	cout << "Nam hoc: " << aca.nienKhoa << '\n';
+	printClass(aca.APCS, 2);
+	printClass(aca.CLC, 2);
+	printClass(aca.CTT, 5);
+}
