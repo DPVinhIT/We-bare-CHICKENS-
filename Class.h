df40c54bb5d2f4e1212f1420d8cf2cfde0f3170d
Huỳnh Trọng Viên <huynhtrongvien2005@gmail.com>
2024-05-14 23:52:45 +0700

    Update Class.h

--- Class.h
+++ Class.h
@@ -10,6 +10,7 @@ struct Clas
 };
 int chooseClass();
 void readFileStudent(char fileName[], Clas& Lop, int& n);
+bool findSubstring(string mainString, string subString);
 Clas* createClass(string file_name[], AcaYear& aca, int& sl, int idx);
 void printClass(Clas* cls,int n);
 #endif
