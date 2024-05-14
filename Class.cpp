ad4999cbec04d0a93be9b542ef61c3b40e4adc42
Huỳnh Trọng Viên <huynhtrongvien2005@gmail.com>
2024-05-14 23:52:17 +0700

    Update Class.cpp

--- Class.cpp
+++ Class.cpp
@@ -1,5 +1,5 @@
 #include"Class.h"
-
+//đọc file lưu là class
 void readFileStudent(char fileName[], Clas& Lop, int& n)
 {
 	n = 0;
@@ -36,7 +36,59 @@ void readFileStudent(char fileName[], Clas& Lop, int& n)
 	fin.close();
 	cout << "Doc file thanh cong" << endl;
 }
-
+//khởi tạo lớp học từ tệp đã nhập
+Clas* createClass(string file_name[], AcaYear& aca, int& sl, int idx)
+{
+	int n;
+	string class_name[9];
+	for (int i = 0; i < 9; i++) {
+		class_name[i] = file_name[i].substr(0, file_name[i].find_last_of("."));
+	}
+	int j = 0;
+	if (idx == 1) {
+		aca.APCS = new Clas[2];
+		for (int i = 0; i < 9; i++) {
+			if (findSubstring(class_name[i], "APCS")) {
+				aca.APCS[j].nameClass = class_name[i];
+				readFileStudent(file_name[i], aca.APCS[j], n);
+				j++;
+			}
+		}
+		return aca.APCS;
+	}
+	else if (idx == 2) {
+		aca.CLC = new Clas[2];
+		for (int i = 0; i < 9; i++) {
+			if (findSubstring(class_name[i], "CLC")) {
+				aca.CLC[j].nameClass = class_name[i];
+				readFileStudent(file_name[i], aca.CLC[j], n);
+				j++;
+			}
+		}
+		return aca.CLC;
+	}
+	else if (idx == 3) {
+		aca.CTT = new Clas[5];
+		for (int i = 0; i < 9; i++) {
+			if (findSubstring(class_name[i], "CTT")) {
+				aca.CTT[j].nameClass = class_name[i];
+				readFileStudent(file_name[i], aca.CTT[j], n);
+				j++;
+			}
+		}
+		return aca.CTT;
+	}
+}
+//do khớp chuỗi con
+bool findSubstring(string mainString, string subString) {
+	if (mainString.find(subString) != string::npos) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+//in ra màn hình lớp học
 void printClass(Clas* cls, int n)
 {
 	if (cls == NULL) {
