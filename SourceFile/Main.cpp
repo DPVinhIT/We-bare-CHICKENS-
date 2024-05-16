40f95c4b69df320dbb4cb87a32ad3d25a2497a84
Đỗ Phước Vinh <162714593+DPVinhIT@users.noreply.github.com>
2024-05-16 22:02:13 +0700

    Update Main.cpp

--- SourceFile/Main.cpp
+++ SourceFile/Main.cpp
@@ -10,7 +10,7 @@ int main() {
 	node APCS1 = NULL;
 	node APCS2 = NULL;
 	createAcademicYear(2023);
-	string file_name[] = { "23CTT1.txt", "23CTT2.txt", "23CTT3.txt", "23CTT4.txt", "23CTT5.txt", "23CLC01.txt", "23CLC02.txt", "23APCS1.txt", "23APCS2.txt" };
+	string file_name[] = { "23CTT1.csv", "23CTT2.csv", "23CTT3.csv", "23CTT4.csv", "23CTT5.csv", "23CLC01.csv", "23CLC02.csv", "23APCS1.csv", "23APCS2.csv" };
 	int n1 = chooseClass();
 	AcaYear aca;
 	aca.CTT = new Clas[5];
@@ -24,14 +24,14 @@ int main() {
 	cout<<"------------"<<endl;
 	for(int i = 0; i<5; i++){
 		node tmp = makeNode(a[0].sinhVien[i]);
-		addNode(APCS1, tmp);
+		addNodeSV(APCS1, tmp);
 	}
 	printNode(APCS1);
 	cout<<"\n"<<a[1].nameClass<<endl;
 	cout<<"------------"<<endl;
 	for(int i = 0; i<5; i++){
 		node tmp = makeNode(a[1].sinhVien[i]);
-		addNode(APCS2, tmp);
+		addNodeSV(APCS2, tmp);
 	}
 	printNode(APCS2);
 	}
@@ -40,14 +40,14 @@ int main() {
 	cout<<"------------"<<endl;
 	for(int i = 0; i<5; i++){
 		node tmp = makeNode(a[0].sinhVien[i]);
-		addNode(CLC01, tmp);
+		addNodeSV(CLC01, tmp);
 	}
 	printNode(CLC01);
 	cout<<"\n"<<a[1].nameClass<<endl;
 	cout<<"------------"<<endl;
 	for(int i = 0; i<5; i++){
 		node tmp = makeNode(a[1].sinhVien[i]);
-		addNode(CLC02, tmp);
+		addNodeSV(CLC02, tmp);
 	}
 	printNode(CLC02);
 	}
@@ -56,40 +56,40 @@ int main() {
 	cout<<"------------"<<endl;
 	for(int i = 0; i<5; i++){
 		node tmp = makeNode(a[0].sinhVien[i]);
-		addNode(CTT1, tmp);
+		addNodeSV(CTT1, tmp);
 	}
 	printNode(CTT1);
 	cout<<"\n"<<a[1].nameClass<<endl;
 	cout<<"------------"<<endl;
 	for(int i = 0; i<5; i++){
 		node tmp = makeNode(a[1].sinhVien[i]);
-		addNode(CTT2, tmp);
+		addNodeSV(CTT2, tmp);
 	}
 	printNode(CTT2);
 	cout<<"\n"<<a[2].nameClass<<endl;
 	cout<<"------------"<<endl;
 	for(int i = 0; i<5; i++){
 		node tmp = makeNode(a[2].sinhVien[i]);
-		addNode(CTT3, tmp);
+		addNodeSV(CTT3, tmp);
 	}
 	printNode(CTT3);
 	cout<<"\n"<<a[3].nameClass<<endl;
 	cout<<"------------"<<endl;
 	for(int i = 0; i<5; i++){
 		node tmp = makeNode(a[3].sinhVien[i]);
-		addNode(CTT4, tmp);
+		addNodeSV(CTT4, tmp);
 	}
 	printNode(CTT4);
 	cout<<"\n"<<a[4].nameClass<<endl;
 	cout<<"------------"<<endl;
 	for(int i = 0; i<5; i++){
 		node tmp = makeNode(a[4].sinhVien[i]);
-		addNode(CTT5, tmp);
+		addNodeSV(CTT5, tmp);
 	}
 	printNode(CTT5);
 	}
 	Student sv1;
-	cout<<"\nNhap thong tin sinh vien can them: "<<endl;
+	cout<<"\nNhap thong tin sinh vien can them "<<":"<<endl;
 	inputAStudent(sv1);
 	node tmp2 = makeNode(sv1);
 	int choice2;
@@ -98,14 +98,14 @@ int main() {
 		cin>>choice2;
 		switch(choice2){
 			case 1:{
-				addNode(APCS1, tmp2);
-				cout<<"\nSau khi them sinh vien moi vao danh sach: "<<a[0].nameClass<<endl;
+				addNodeSV(APCS1, tmp2);
+				cout<<"\nSau khi them sinh vien moi vao danh sach "<<a[0].nameClass<<":"<<endl;
 				printNode(APCS1);
 				break;
 			}
 			case 2:{
-				addNode(APCS2, tmp2);
-				cout<<"\nSau khi them sinh vien moi vao danh sach: "<<a[1].nameClass<<endl;
+				addNodeSV(APCS2, tmp2);
+				cout<<"\nSau khi them sinh vien moi vao danh sach "<<a[1].nameClass<<":"<<endl;
 				printNode(APCS2);
 				break;
 			}
@@ -118,14 +118,14 @@ int main() {
 		cin>>choice2;
 		switch(choice2){
 			case 1:{
-				addNode(CLC01, tmp2);
-				cout<<"\nSau khi them sinh vien moi vao danh sach: "<<a[0].nameClass<<endl;
+				addNodeSV(CLC01, tmp2);
+				cout<<"\nSau khi them sinh vien moi vao danh sach "<<a[0].nameClass<<":"<<endl;
 				printNode(CLC01);
 				break;
 			}
 			case 2:{
-				addNode(CLC02, tmp2);
-				cout<<"\nSau khi them sinh vien moi vao danh sach: "<<a[1].nameClass<<endl;
+				addNodeSV(CLC02, tmp2);
+				cout<<"\nSau khi them sinh vien moi vao danh sach "<<a[1].nameClass<<":"<<endl;
 				printNode(CLC02);
 				break;
 			}
@@ -137,38 +137,39 @@ int main() {
 		cin>>choice2;
 		switch(choice2){
 			case 1:{
-				addNode(CTT1, tmp2);
-				cout<<"\nSau khi them sinh vien moi vao danh sach: "<<a[0].nameClass<<endl;
+				addNodeSV(CTT1, tmp2);
+				cout<<"\nSau khi them sinh vien moi vao danh sach "<<a[0].nameClass<<":"<<endl;
 				printNode(CTT1);
 				break;
 			}
 			case 2:{
-				addNode(CTT2, tmp2);
-				cout<<"\nSau khi them sinh vien moi vao danh sach: "<<a[1].nameClass<<endl;
+				addNodeSV(CTT2, tmp2);
+				cout<<"\nSau khi them sinh vien moi vao danh sach "<<a[1].nameClass<<":"<<endl;
 				printNode(CTT2);
 				break;
 			}
 			case 3:{
-				addNode(CTT3, tmp2);
-				cout<<"\nSau khi them sinh vien moi vao danh sach: "<<a[2].nameClass<<endl;
+				addNodeSV(CTT3, tmp2);
+				cout<<"\nSau khi them sinh vien moi vao danh sach "<<a[2].nameClass<<":"<<endl;
 				printNode(CTT3);
 				break;
 			}
 			case 4:{
-				addNode(CTT4, tmp2);
-				cout<<"\nSau khi them sinh vien moi vao danh sach: "<<a[3].nameClass<<endl;
+				addNodeSV(CTT4, tmp2);
+				cout<<"\nSau khi them sinh vien moi vao danh sach "<<a[3].nameClass<<":"<<endl;
 				printNode(CTT4);
 				break;
 			}
 			case 5:{
-				addNode(CTT5, tmp2);
-				cout<<"\nSau khi them sinh vien moi vao danh sach: "<<a[4].nameClass<<endl;
+				addNodeSV(CTT5, tmp2);
+				cout<<"\nSau khi them sinh vien moi vao danh sach "<<a[4].nameClass<<":"<<endl;
 				printNode(CTT5);
 				break;
 			}
 	}
-	return 0;
+	
 }
+	return 0;
 }
 
  
