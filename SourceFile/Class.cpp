437c09acc109314eec4dc476b7248d92bf09f0a0
Jinn-645 <162824573+Jinn-645@users.noreply.github.com>
2024-05-17 03:07:48 +0700

    Update Class.cpp

--- SourceFile/Class.cpp
+++ SourceFile/Class.cpp
@@ -1,5 +1,18 @@
 #include"Class.h"
 
+void splitName(Student& st)
+{
+	int first, last;
+	if (st.fullName.find(" ") == string::npos)
+	{
+		return;
+	}
+	first = st.fullName.find(" ");
+	last = st.fullName.find_last_of(" ");
+	st.lastName = st.fullName.substr(0, first);
+	st.firstName = st.fullName.substr(last + 1);
+}
+
 void readFileStudent(string fileName, Clas& Lop, int& n)
 {
 	n = 0;
@@ -16,14 +29,14 @@ void readFileStudent(string fileName, Clas& Lop, int& n)
 		getline(fin, temp);
 		n++;
 	}
-	fin.seekg(0,ios::beg);
+	fin.seekg(0, ios::beg);
 	Lop.sinhVien = new Student[n];
 
 	for (int i = 0; i < n; i++)
 	{
-		getline(fin, Lop.sinhVien[i].studentID, '-');
-		getline(fin, Lop.sinhVien[i].fullName, '-');
-		getline(fin, Lop.sinhVien[i].Gender, '-');
+		getline(fin, Lop.sinhVien[i].studentID, ';');
+		getline(fin, Lop.sinhVien[i].fullName, ';');
+		getline(fin, Lop.sinhVien[i].Gender, ';');
 		fin.ignore();
 		fin >> Lop.sinhVien[i].DoB.day;
 		fin.ignore();
@@ -32,6 +45,7 @@ void readFileStudent(string fileName, Clas& Lop, int& n)
 		fin >> Lop.sinhVien[i].DoB.year;
 		fin.ignore();
 		getline(fin, Lop.sinhVien[i].socialID, '\n');
+		splitName(Lop.sinhVien[i]);
 	}
 	fin.close();
 }
