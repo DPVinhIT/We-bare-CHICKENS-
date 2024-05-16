30ca513ce22ff6594909c25935603fbda15f37f9
Jinn-645 <162824573+Jinn-645@users.noreply.github.com>
2024-05-16 19:10:27 +0700

    Create Courses.h

---
+++ Courses.h
@@ -0,0 +1,22 @@
+#ifndef Courses_h
+#define Courses_h
+
+#include "AcademicYear.h"
+
+struct calendar
+{
+	string Day[6]={"MON","TUE","WED","THU","FRI","SAT"};
+	string Time[4] = {"S1:7h30","S2:9h30","S3:13h30","S4:15h30"};
+};
+
+struct Course
+{
+	string courseID;
+	string courseName;
+	Clas cls;
+	string teacher;
+	int acaCrd;
+	int maxStudent;
+	calendar cld;
+};
+#endif
