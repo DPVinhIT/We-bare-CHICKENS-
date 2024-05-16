dde7c4248b792705376b557779c7909c4fd38ae5
Đỗ Phước Vinh <162714593+DPVinhIT@users.noreply.github.com>
2024-05-16 22:03:09 +0700

    Update Functions.h

--- Header/Functions.h
+++ Header/Functions.h
@@ -8,13 +8,14 @@ Clas* createClass(string file_name[], AcaYear &aca, int& sl, int idx);
 int chooseClass();
 void readFileStudent(string fileName, Clas& Lop, int& n);
 void printClass(Clas* cls,int n);
-void printStudent(Student* st, int n);
+void printStudent(Student* &st, int n);
 node makeNode(Student sv);
-void addNode(node &a, node newNode);
+void addNodeSV(node &a, node newNode);
 void printNode(node a);
 void menu1();
 void menu2();
 void menu3();
 void inputDate(Date& d);
 void inputAStudent(Student& hs);
+void addNodeC(node &a, node newNode);
 #endif
