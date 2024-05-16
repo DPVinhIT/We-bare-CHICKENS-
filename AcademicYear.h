722fed610ea602d4efad23cfc268487890e3c9e8
Jinn-645 <162824573+Jinn-645@users.noreply.github.com>
2024-05-16 19:08:02 +0700

    add Course

--- AcademicYear.h
+++ AcademicYear.h
@@ -9,5 +9,20 @@ struct AcaYear
 	Clas* CLC;
 	Clas* CTT;
 };
+struct calendar
+{
+	string Day[6]={"MON","TUE","WED","THU","FRI","SAT"};
+	string Time[4] = {"S1:7h30","S2:9h30","S3:13h30","S4:15h30"};
+};
 
+struct Course
+{
+	string courseID;
+	string courseName;
+	Clas cls;
+	string teacher;
+	int acaCrd;
+	int maxStudent;
+	calendar cld;
+};
 #endif
