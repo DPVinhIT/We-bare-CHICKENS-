f773ea84ecb31c116348fe705cbf4b0beb8decb0
Huỳnh Trọng Viên <huynhtrongvien2005@gmail.com>
2024-06-04 21:22:51 +0700

    Update Functions.cpp

--- Source/Functions.cpp
+++ Source/Functions.cpp
@@ -1,4 +1,92 @@
 #include "Functions.h"
+
+//Ham in
+void printStudent(Student st) {
+	cout << "MSSV: " << st.studentID << endl;
+	cout << "Ho va ten: " << st.fullName << endl;
+	cout << "Gioi tinh: " << st.Gender << endl;
+	cout << "Ngay sinh: " << st.DoB.day << '/' << st.DoB.month << '/' << st.DoB.year << endl;
+	cout << "CCCD/CMND: " << st.socialID << endl;
+}
+void printListStudent(ListStudent lst) {
+	if (lst.Head == NULL) {
+		cout << "Chua co hoc sinh." << endl;
+		return;
+	}
+	NodeStudent* nodeSt = lst.Head;
+	cout << "----------------------------------------------\n";
+	while (nodeSt != NULL) {
+		printStudent(nodeSt->sv);
+		cout << "----------------------------------------------\n";
+		nodeSt = nodeSt->Next;
+	}
+}
+void printClass(Clas cls) {
+	cout << "Lop: " << cls.nameClass << endl;
+	printListStudent(cls.lst);
+}
+void printListClass(ListClass lcls) {
+	if (lcls.Head == NULL) {
+		cout << "Chua co lop hoc." << endl;
+		return;
+	}
+	NodeClass* nodeCls = lcls.Head;
+	cout << "\n----------------------------------------------\n";
+	cout << "----------------------------------------------\n";
+
+	while (nodeCls != NULL) {
+		printClass(nodeCls->cls);
+		cout << "----------------------------------------------\n";
+		cout << "----------------------------------------------\n\n";
+		nodeCls = nodeCls->Next;
+	}
+}
+void printCourse(Course crs) {
+	cout << "Ma khoa hoc: " << crs.courseID << endl;
+	cout << "Ten khoa hoc: " << crs.courseName << endl;
+	cout << "Danh sach hoc sinh trong khoa hoc: " << endl;
+	printListStudent(crs.sv);
+	cout << "So tin chi: " << crs.acaCrd;
+	cout << "So luong sinh vien toi da: " << crs.maxStudent << endl;
+	cout << "Giao vien: " << crs.teacher << endl;
+	cout << "Lich hoc: " << crs.cld.Day << ':' << crs.cld.Time << endl;
+}
+void printListCourse(ListCourse lcrs) {
+	if (lcrs.Head == NULL) {
+		cout << "Chua co khoa hoc." << endl;
+		return;
+	}
+	NodeCourse* nodeCrs = lcrs.Head;
+	while (nodeCrs != NULL) {
+		cout << "************************************************\n";
+		printCourse(nodeCrs->crs);
+		cout << "************************************************\n";
+		nodeCrs = nodeCrs->Next;
+	}
+}
+void printSemester(Semester smt) {
+	cout << "Ngay bat dau: " << smt.begin.day << '/' << smt.begin.month << '/' << smt.begin.year << endl;
+	cout << "Ngay ket thuc: " << smt.end.day << '/' << smt.end.month << '/' << smt.end.year << endl;
+	printListCourse(smt.lcrs);
+}
+void printListSemester(ListSeme lsmt) {
+	if (lsmt.Head == NULL) {
+		cout << "Chua co hoc ky.\n";
+		return;
+	}
+	NodeSeme* nodeSmt = lsmt.Head;
+	while (nodeSmt != NULL) {
+		cout << "************************************************\n";
+		printSemester(nodeSmt->smt);
+		cout << "************************************************\n";
+		nodeSmt = nodeSmt->Next;
+	}
+}
+void printAca(Academy aca) {
+	cout << "Nam hoc: " << aca.begin << '-' << aca.end << endl;
+	printListSemester(aca.lsm);
+}
+
 void createAcademicYear(int yearBegin) {
 	cout << "\nNam hoc: " << yearBegin << " - " << yearBegin + 1 << endl;
 }
