061aeb0dccceb0a66c7d61c646184caddd8a5422
Jinn-645 <162824573+Jinn-645@users.noreply.github.com>
2024-06-27 00:02:38 +0700

    Almost completed

--- Header/Functions.h
+++ Header/Functions.h
@@ -2,107 +2,72 @@
 #define Functions_h
 
 #include"Struct.h"
+
 bool laNgayHopLe(Date t);
 int dayOfMonth(Date d);
-void createAca(Academy acd);
-int chooseClass();
-int chooseSemester();
 void splitName(Student& st);
+
+//Hàm xử lí dữ liệu student
 NodeStudent* createNodeStudent(Student sv);
-NodeClass* createNodeClass(Clas cls);
+NodeStudent* findStudentByPos(ListStudent lStd, int pos);
+NodeStudent* findStudentByID(ListStudent lst, string ID);
 void addNodeStudent(ListStudent& lst, NodeStudent* st);
-void addNodeClass(ListClass& lcls, NodeClass* cls);
-void readFileStudent(string fileName[], ListClass& lcls);
-Student inputStudent();
-void InputCourse(Course& cou);
-void printStudent(Student st);
-void printListStudent(ListStudent lst);
-void printClass(Clas cls);
-void printListClass(ListClass lcls);
-void printCourse(Course crs);
-void printListCourse(ListCourse lcrs);
-void printSemester(Semester smt);
-void printListSemester(ListSeme lsmt);
-void printAca(Academy aca);
 void removeFirst(ListStudent& lst);
 void removeLast(ListStudent& lst);
 int getSize(ListStudent lst);
+int countStudent(ListStudent lStd);
 void removeStudent(ListStudent& lst, int pos);
 void removeStudent_MSSV(ListStudent& lst, string studentID);
-void updateCourse(Course& crs);
-void writeCoure(string fileName, Course cour);
-void printListStudentScore(Course crs);
-void printListCourseScore(Course crs);
-void outputScore(Course a);
-int PrintScore();
-NodeAccount* createNodeAccount(Account acc);
-void addNodeAccount(ListAccount& lac, NodeAccount* acc);
-void UploadAccount(string fileName, ListAccount& acc);
-void printListAccount(ListAccount lac);
-NodeAccount* Login(ListAccount lac, string user, string pass);
-void printInforOfCrs(Course crs);
-void printTheStudentCourses(string mssv, ListCourse lstCrs);
-
-int countStudent(ListStudent lStd);
-
-int countClass(ListClass lCls);
-
-NodeStudent* findStudentByPos(ListStudent lStd, int pos);
-
-NodeStudent* findStudentByID(ListStudent lst, string ID);
-
-NodeClass* findClassByPos(ListClass lcls, int pos);
-
-NodeClass* findClassByName(ListClass lcls, string name);
-
 void readFileCSV(string fileName, ListStudent& lst, bool& check);
 
+//Hàm xử lí dữ liệu clas
+NodeClass* createNodeClass(Clas cls);
+NodeClass* findClassByPos(ListClass lcls, int pos);
+NodeClass* findClassByName(ListClass lcls, string name);
+void addNodeClass(ListClass& lcls, NodeClass* cls);
+void readFileStudent(string fileName[], ListClass& lcls);
+int countClass(ListClass lCls);
 ListClass lstClsInAYear(Academy aca, ListClass lst, int year);
 
-int check_Seme(Semester smt, Date cur);
+//Hàm xử lí tài khoản
+NodeAccount* createNodeAccount(Account acc);
+void addNodeAccount(ListAccount& lac, NodeAccount* acc);
+NodeAccount* Login(ListAccount lac, string user, string pass);
+void changePasswordInListAccount(ListAccount& lAcc, Account Acc);
+void UploadAccount(string fileName, ListAccount& acc);
+void printListAccount(ListAccount lac);
 
+//Hàm xử lí khóa hoc
+NodeCourse* findCourseByPos(ListCourse lcrs, int pos);
+NodeCourse* findCourseByID(ListCourse lcrs, string courseID);
+NodeCourse* createNodeCourse(Course crs);
+void addNodeCourse(ListCourse& lcrs, NodeCourse* crs);
+void removeStudentOfCourse(ListCourse& lcrs, string courseID, string studentID);
+void removeFirst(ListCourse& lcr);
+void removeLast(ListCourse& lcr);
+void removeCourse(ListCourse& lcr, int pos);
+void removeCourse_IDCourse(ListCourse& lcr, string IDcourse);
+int countCourse(ListCourse lCrs);
+ListCourse RegisteredCourse(string StudentID);
+ListCourse CourseOfClass(string className);
+void writeCoure(string fileName, Course cour);
+
+//Hàm xử lí thời gian
+int checkSeme();
 int checkSemester(Date cur);
 
-int countCourse(ListCourse lCrs);
-
-NodeCourse* findCourseByPos(ListCourse lcrs, int pos);
-
-NodeCourse* findCourseByID(ListCourse lcrs, string courseID);
-
+//Hàm xử lí năm học và học kì
 NodeSeme* createNodeSeme(Semester sms);
-
 void addNodeSeme(ListSeme& lsm, NodeSeme* ns);	
+NodeAca* createNodeAcademy(Academy acm);
+void addNodeAcademy(ListAca& lta, NodeAca* aca);
+bool checkSemester(int stt, Date start, Date end, NodeAca* CurAcademy);
 
-NodeCourse* createNodeCourse(Course crs);
-
-void addNodeCourse(ListCourse& lcrs, NodeCourse* crs);
-
-NodeStudent* check_Course_and_MSSV(ListCourse lcr, string IDcourse, string MSSV);
-
-void removeStudentOfCourse(ListCourse& lcrs, string courseID, string studentID);
-
-void inputScore_Student(ListCourse& lcr, string IDcourse, string MSSV);
-
-void removeFirst(ListCourse& lcr);
-
-void removeLast(ListCourse& lcr);
-
-void removeCourse(ListCourse& lcr, int pos);
-
-void removeCourse_IDCourse(ListCourse& lcr, string IDcourse);
-
-void changePassword(Account& acc);
-
-void changePasswordInListAccount(ListAccount& lAcc, Account Acc);
-
+//Đọc file bảng điểm
 void readFileScoreboard(string fileName, Course& crs);
 
-NodeAca* createNodeAcademy(Academy acm);
-
-void addNodeAcademy(ListAca& lta, NodeAca* aca);
-
-double GPA_one_Seme(Semester sms, string StudentID);
-
-double* GPA_three_Seme(ListSeme lsm, string StudentID);
+//Tính gpa
+double GpaOfSemester(string StudentID, int& numCredit);
+double GpaTotal(string StudentID);
 
 #endif
