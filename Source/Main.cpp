e08279b883a97b77f349ef5dcf2839fb8c6e1af8
Jinn-645 <162824573+Jinn-645@users.noreply.github.com>
2024-06-27 23:08:12 +0700

    Almost completed

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -20,9 +20,10 @@ int main()
 {
 
 	ListClass cls;
-	string fileName[] = { "23CTT1.csv", "21CTT2.csv", "23CTT3.csv", "23CTT4.csv", "23CTT5.csv", "23CLC01.csv",
-	"23CLC02.csv", "23APCS1.csv", "23APCS2.csv" };
-	readFileStudent(fileName, cls);
+	string fileName[] = { "23CTT1.csv", "23CTT2.csv", "23CTT3.csv", "23CTT4.csv", "23CTT5.csv", "23CLC01.csv",
+	"23CLC02.csv", "23APCS1.csv", "23APCS2.csv","20CTT1.csv","20CTT2.csv","21CTT1.csv","21CTT2.csv","22CTT1.csv","22CTT2.csv"};
+
+	readFileStudent(fileName, cls, sizeof(fileName) / sizeof(fileName[0]));
 	SetConsoleTitle("WeBearCHICKEN");
 
 	//ShowCur(false);
@@ -42,7 +43,7 @@ int main()
 		CurSemester = CurAcademy->acm.lsm.Head;
 	}
 
-	//menuLogin(cls, CurAcademy);
+	menuLogin(cls, CurAcademy);
 
 	_getch();
 	GoTo(0, 45);
